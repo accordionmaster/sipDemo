26ace160b4f55b2c3568f82ea1faa634c8487af8
wangxp <accordionmaster@163.com>
2019-10-24 20:39:56 +0800

    init the projects

---
+++ prettyblog/src/main/java/com/wxp/prettyblog/PrettyblogApplication.java
@@ -0,0 +1,13 @@
+package com.wxp.prettyblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrettyblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrettyblogApplication.class, args);
+    }
+
+}
---
+++ prettyblog/src/test/java/com/wxp/prettyblog/PrettyblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wxp.prettyblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrettyblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
